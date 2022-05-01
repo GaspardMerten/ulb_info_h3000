13b98308db59c6f940639e1bb2011fcbe28eee44
jZong <jonathanstefanov1@gmail.com>
2022-05-01 17:00:43 +0200

    Added communes addresses and function

---
+++ communes.py
@@ -0,0 +1,14 @@
+import csv
+
+
+def get_all_addresses():
+    addresses = []
+    with open('communes.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            addresses.append(row[1])
+
+    return addresses
+
+
+print(str(get_all_addresses()))
