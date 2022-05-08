a598c89c37d19e57bda972a47009ac4b6c5c8059
jZong <jonathanstefanov1@gmail.com>
2022-05-08 12:11:32 +0200

    Fitness Testing

--- domain/compute_fitness.py
+++ domain/compute_fitness.py
@@ -4,7 +4,9 @@ from models import GlobalConfig, TruckPath, Truck
 
 def compute_total_fitness(dna, global_config):
     total_fitness = 0
+
     groups = divide_by_truck(dna)
+
     for group_dna in groups:
         truck = index_to_truck(group_dna, global_config)
         total_fitness += compute_truck_fitness(truck)
@@ -15,26 +17,38 @@ def compute_total_fitness(dna, global_config):
 def index_to_truck(group_dna, global_config: GlobalConfig):
     total_path = []
 
-    for i in range(len(group_dna) - 1):
-        place1 = global_config.places[i]
-        place2 = global_config.places[i + 1]
+    def _add_truck_path_from_places(place_1, place_2):
         truck_path = TruckPath(
-            place1,
-            place2,
-            get_distance_between(place1, place2),
-            place1.money
+            place_1,
+            place_2,
+            global_config.get_distance_between(place_1, place_2),
+            place_1.money
         )
 
         total_path.append(truck_path)
+
+    place1 = global_config.places[0]
+    place2 = global_config.places[group_dna[0]]
+
+    _add_truck_path_from_places(place1, place2)
+
+    for i in range(len(group_dna) - 1):
+        place1 = global_config.places[group_dna[i]]
+        place2 = global_config.places[group_dna[i + 1]]
+
+        _add_truck_path_from_places(place1, place2)
+
+    place1 = global_config.places[group_dna[-1]]
+    place2 = global_config.places[0]
+    _add_truck_path_from_places(place1, place2)
 
     return Truck(total_path)
 
 
 def compute_truck_fitness(truck: Truck) -> float:
     fitness = 0
+
     for path in truck.paths:
-        print(path.distance)
-        print(path.money)
         fitness += path.distance + path.distance * path.money
 
     return fitness
--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from domain import geo
+from domain.compute_fitness import compute_total_fitness
+from domain.geo import compute_distance_between_all_places
 from models import GlobalConfig, Place
 from utils import csv
-from domain.compute_fitness import *
-
-MONEY_PER_RESIDENT = 10
+from utils.compute_global_config import get_global_config
 
 
 def main():
-    places = []
 
-    for name, address, population in csv.get_rows_from_csv('assets/town_halls.csv'):
-        long, lat = geo.search(address)
-
-        total_money_in_town_hall = int(population) * MONEY_PER_RESIDENT
-
-        places.append(Place(name, long, lat, total_money_in_town_hall))
-
-    global_config = GlobalConfig(places=places)
-
-    print(global_config)
-    compute_total_fitness([0, 3, 1, 2, 4, 7, 9, 6, 8, 5, 3, 7], global_config)
+    global_config = get_global_config()
+    print(compute_total_fitness([0, 3, 1, 2, 4, 7, 9, 6, 8, 5, 3, 7], global_config))
 
 
 if __name__ == '__main__':
