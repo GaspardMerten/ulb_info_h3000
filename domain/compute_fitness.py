f52bc62ef8edaf5ae986ba3f0eda7f4f170b7152
jZong <jonathanstefanov1@gmail.com>
2022-05-08 12:27:12 +0200

    Finished documentation for the fitness

--- domain/compute_fitness.py
+++ domain/compute_fitness.py
@@ -2,7 +2,14 @@ from domain.geo import get_distance_between
 from models import GlobalConfig, TruckPath, Truck
 
 
-def compute_total_fitness(dna, global_config):
+def compute_total_fitness(dna, global_config) -> float:
+    """
+    Takes a DNA and a global configuration, and returns the total fitness of the DNA
+
+    :param dna: The DNA of the model
+    :param global_config: the configuration file contating the list of the places
+    :return: The total fitness of the DNA.
+    """
     total_fitness = 0
 
     groups = divide_by_truck(dna)
@@ -14,7 +21,17 @@ def compute_total_fitness(dna, global_config):
     return total_fitness
 
 
-def index_to_truck(group_dna, global_config: GlobalConfig):
+def index_to_truck(group_dna, global_config: GlobalConfig) -> Truck:
+    """
+    Based on the information in the DNA of the truck, creating a truck object containing the list of path that if
+    will go through
+
+    :param group_dna: This is the DNA of the group. It's a list of integers, where each integer represents the index of a
+    place in the global_config.places list
+    :param global_config: GlobalConfig
+    :type global_config: GlobalConfig
+    :return: The truck inferred by the DNA group
+    """
     total_path = []
 
     def _add_truck_path_from_places(place_1, place_2):
@@ -46,6 +63,13 @@ def index_to_truck(group_dna, global_config: GlobalConfig):
 
 
 def compute_truck_fitness(truck: Truck) -> float:
+    """
+    The fitness of a truck is the sum of the distances of all its paths, plus the sum of the distances of all its paths
+    multiplied by the money of all its paths
+
+    :param truck: the truck for which to compute the fitness
+    :return: The fitness of the truck.
+    """
     fitness = 0
 
     for path in truck.paths:
@@ -55,6 +79,14 @@ def compute_truck_fitness(truck: Truck) -> float:
 
 
 def divide_by_truck(dna):
+    """
+    Takes a DNA sequence and returns a list of three DNA sequences, each of which is a subsequence of the original.
+    It is used in order to divide the whole DNA in a group of 3 DNA's. It must be noted that the first number (0)
+    always represents the National Bank and that the last two represent where to cut the groups.
+
+    :param dna: the DNA string
+    :return: A list of lists.
+    """
     group2_beginning = dna[-2] + 1
     group3_beginning = dna[-1] + 1
 
--- tests/test_fitness.py
+++ tests/test_fitness.py
@@ -6,6 +6,7 @@ from utils import csv
 from domain.compute_fitness import divide_by_truck, index_to_truck
 from utils.compute_global_config import get_global_config
 
+
 class TestFitness(TestCase):
     dna = [0, 3, 1, 2, 4, 7, 9, 6, 8, 5, 3, 7]
 
@@ -14,6 +15,5 @@ class TestFitness(TestCase):
         assert division == [[3, 1, 2], [4, 7, 9, 6], [8, 5]]
 
     def test_index_to_truck(self):
-        truck = index_to_truck([3, 1, 2], get_global_config())
+        truck = index_to_truck([8, 5], get_global_config())
         print(truck.paths)
-
