ac66145dfe8458372b6d04711c335d53881503e4
Gaspard Merten <gaspard.mp.work@gmail.com>
2022-05-08 11:31:39 +0200

    Fixing geo & town_halls

--- domain/geo.py
+++ domain/geo.py
@@ -53,7 +53,8 @@ def compute_distance_between_all_places(places: List[Place]) -> DistancesMap:
     results: DistancesMap = {}
 
     for count, place_one in enumerate(places):
-        for place_two in places[count + 1:]:
-            results[(place_one, place_two)] = get_distance_between(place_one, place_two)
+        for place_two in places:
+            if place_one != place_two:
+                results[(place_one, place_two)] = get_distance_between(place_one, place_two)
 
     return results
