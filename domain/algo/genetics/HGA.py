2a1e23518ab657fb7977debbca9aa1d8649bef96
jZong <jonathanstefanov1@gmail.com>
2022-05-15 13:46:45 +0200

    Finished child 1 and 2

--- domain/algo/genetics/HGA.py
+++ domain/algo/genetics/HGA.py
@@ -1,16 +1,22 @@
 import random
-from typing import List, Dict
+from typing import List, Dict, Tuple, Any
 from domain.algo.genetics import INaturalSelection
+from domain.dna import extract_fragments_from_dna
 from models import DNA
 
 
-def get_latter_city(dna: DNA, k):
-    if len(dna) == k:
-        return dna[1]
+def get_latter_city(dna, k):
+    print(k)
+    print("len" + str(len(dna)))
+    print(dna)
+
+    position = dna.index(k)
+
+    if len(dna) - 1 == position:
+        return dna[0]
     else:
-        print(dna)
-        print(k)
-        return dna[k + 1]
+
+        return dna[position + 1]
 
 
 def get_former_city(dna: DNA, k):
@@ -20,11 +26,12 @@ def get_former_city(dna: DNA, k):
         return dna[k - 1]
 
 
+# noinspection PyTypeChecker
 class HGA(INaturalSelection):
     MARK = "LATTER"  # Can be definied as LATTER or FORMER
 
     def select_parents(
-        self, generation: List[DNA], fitness: Dict[int, int]
+            self, generation: List[DNA], fitness: Dict[int, int]
     ) -> List[int]:
         ordered_fitness: List[int] = list(
             dict(sorted(fitness.items(), key=lambda item: item[1])).keys()
@@ -41,27 +48,118 @@ class HGA(INaturalSelection):
 
         return selected_parents_indexes
 
-    def generate_children_from_parents(
-        self, parent_one: DNA, parent_two: DNA
-    ) -> List[DNA]:
-        pass
+    def decode(self, dna: DNA):
+        groups = extract_fragments_from_dna(dna)
+        decoded_dna = []
 
-    def hga_algo(self, parent_one: DNA, parent_two: DNA):
-        parent_one_length = len(parent_one)
-        k = random.randint(1, parent_one_length)
+        for group in groups:
+            decoded_dna = decoded_dna + [0] + list(group)
+
+        return decoded_dna
+
+    def rationalize(self, dna: DNA) -> tuple[int | Any, ...]:
+        dna_list = list(dna)
+        a = []
+
+        x = 0
+
+        # Sending all first numbers before first zero to the end
+        for v in dna_list:
+            if v == 0:
+                break
+            else:
+                x += 1
+                a.append(v)
+        dna_list = dna_list[x:] + a
+
+        # Deleting consecutive zeros
+        zero_index = None
+        for i in range(0, len(dna_list) - 1):
+            if dna_list[i] == 0 and dna_list[i+1] == 0:
+                zero_index = i
+                break
+
+        if zero_index:
+            random_index = list(range(2, len(dna_list)))
+            dna_list.pop(zero_index)
+
+            del random_index[zero_index:zero_index+2]
+
+            final_index = random.choice(random_index)
+            dna_list.insert(final_index, 0)
+
+        positions = []
+        for index, value in enumerate(dna_list):
+            if index != 0 and value == 0:
+                positions.append(index - 1 - len(positions))
+        print("dsds" + str(dna_list))
+
+        for i in range(3):
+            dna_list.remove(0)
+        dna_list.insert(0, 0)
+
+
+        dna_list = dna_list + positions
+
+        return tuple(dna_list)
+
+    def generate_child1(self, parent_one, parent_two):
+        parent1_part1 = list(parent_one[1:20])
+        parent2_part1 = list(parent_two[1:20])
+
+        # Generation of child1
+        child1_0 = [0]
+        child1_body = self.hga_algo(parent1_part1, parent2_part1)
+        # Randomly choosing a group delimited from one of the parents
+        if random.randint(0, 1) == 0:
+            child1_tail = list(parent_one[20:22])
+        else:
+            child1_tail = list(parent_two[20:22])
+
+        child1 = tuple(child1_0 + child1_body + child1_tail)
+
+        return child1
+
+    def generate_child2(self, parent_one, parent_two):
+        # Generation of child2
+        parent1_decoded = self.decode(parent_one)
+        parent2_decoded = self.decode(parent_two)
+
+        child2_unrationalized = self.hga_algo(parent1_decoded, parent2_decoded)
+        child2 = self.rationalize(child2_unrationalized)
+        return child2
+
+    def generate_children_from_parents(
+            self, parent_one: DNA, parent_two: DNA
+    ) -> List[DNA]:
+
+        child1 = self.generate_child1(parent_one, parent_two)
+        child2 = self.generate_child2(parent_one, parent_two)
+
+        return child1, child2
+
+    def hga_algo(self, parent_one, parent_two):
+        parent_one_list = list(parent_one)
+        parent_two_list = list(parent_two)
+        parent_one_length = len(parent_one_list)
+        k = random.choice(list(set(parent_one)))
+
+        print("PARENR1" + str(len(parent_one_list)))
 
         result = [k]
 
         while parent_one_length > 1:
             if self.MARK == "LATTER":
-                x = get_latter_city(parent_one, k)
-                y = get_latter_city(parent_two, k)
+                print("parent1")
+                x = get_latter_city(parent_one_list, k)
+                print("parent2")
+                y = get_latter_city(parent_two_list, k)
             else:
-                x = get_former_city(parent_one, k)
-                y = get_former_city(parent_two, k)
+                x = get_former_city(parent_one_list, k)
+                y = get_former_city(parent_two_list, k)
 
-            parent_one.remove(k)
-            parent_two.remove(k)
+            parent_one_list.remove(k)
+            parent_two_list.remove(k)
 
             distance_x_k = self.config.get_distance_between(
                 self.config.places[k], self.config.places[x]
@@ -76,7 +174,8 @@ class HGA(INaturalSelection):
                 k = y
 
             result.append(k)
-            parent_one_length = len(parent_one)
+            parent_one_length = len(parent_one_list)
+            print("result = " + str(result))
 
         return result
 
@@ -85,3 +184,24 @@ class HGA(INaturalSelection):
 
     def apply_mutation(self, dna: DNA) -> None:
         raise NotImplemented()
+
+    def get_boosted_dna(self, dna: DNA) -> DNA:
+        dna_list = list(dna)
+
+        dna_without_group = dna_list[0:20]
+
+        max_dna = dna
+        max_dna_score = compute_total_fitness(dna, self.config)
+        old_dna_score = max_dna_score
+        for i in range(4, 12):
+            for j in range(i, 16):
+                tmp_dna = dna_without_group + [i, j]
+                dna_score = compute_total_fitness(tuple(tmp_dna), self.config)
+
+                if dna_score < max_dna_score:
+                    max_dna_score = dna_score
+                    max_dna = tmp_dna
+        print(
+            f"\nBoosted DNA: \nFrom {old_dna_score} to {max_dna_score}\nImprovement: {round((1 - max_dna_score / old_dna_score) * 100, 2)}%"
+        )
+        return tuple(max_dna)
--- tests/test_hga.py
+++ tests/test_hga.py
@@ -1,13 +1,14 @@
 from unittest import TestCase
-
 from domain.algo.genetics.HGA import HGA
-from domain.compute_fitness import divide_by_truck, dna_fragment_to_truck
+from models import DNA
 from utils.compute_global_config import get_global_config
 
 
 class TestHGAAlgo(TestCase):
-    dna = [0, 3, 1, 2, 4, 7, 9, 6, 8, 5, 3, 7]
-
     def test_hga(self):
-        hga = HGA(get_global_config())
-        print(hga.hga_algo([0, 1, 2, 3, 4, 5], [0, 5, 3, 2, 4, 1]))
+        hga = HGA(get_global_config("../assets/town_halls.csv"))
+        print(hga.generate_children_from_parents(
+            (0, 1, 2, 13, 4, 5, 16, 7, 9, 8, 10, 11, 12, 3, 14, 15, 6, 17, 18, 19, 6, 8),
+            (0, 4, 1, 3, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2, 9))
+        )
+
